a2e5977fd97802e3187bcd8bad0b1c6e8c4cc143
PentagonCoder <harshu33908@gmail.com>
2026-03-17 13:27:22 +0000

    all configurasion done

--- src/app.js
+++ src/app.js
@@ -12,4 +12,12 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+import { router as userRouter } from "./routes/user.routes.js";
+
+// router decleration
+app.use("/api/v1/users", userRouter);
+
+
 export  {app};

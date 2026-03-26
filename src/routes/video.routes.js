8aefeb5a3f0c4c9d48059c2001fb06de29b62cf0
PentagonCoder <harshu33908@gmail.com>
2026-03-26 17:50:27 +0000

    refactor: clean up video routes by removing commented code and improving readability

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -11,7 +11,25 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
+//this is used to protect all the routes in this file, so that only authenticated users can access these routes. It will check for the JWT token in the request header and verify it before allowing access to the routes.
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+
+router.get("/", getAllVideos);
+router.post("/", upload.fields([
+  { name: "videoFile", maxCount: 1 },
+  { name: "thumbnail", maxCount: 1 },
+]), publishAVideo);
+
+// router.route("/:videoId").get(getVideoById).delete(deleteVideo).patch(upload.single("thumbnail"), updateVideo);
+router.get("/:videoId", getVideoById);
+router.delete("/:videoId", deleteVideo);
+router.patch("/:videoId", upload.single("thumbnail"), updateVideo);
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+
+export default router;
+
+
 
 // router
 //   .route("/")
@@ -30,17 +48,3 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 //     ]),
 //     publishAVideo
 //   );
-
-router.get("/", getAllVideos);
-router.post("/", upload.fields([
-  { name: "videoFile", maxCount: 1 },
-  { name: "thumbnail", maxCount: 1 },
-]), publishAVideo);
-
-// router.route("/:videoId").get(getVideoById).delete(deleteVideo).patch(upload.single("thumbnail"), updateVideo);
-router.get("/:videoId", getVideoById);
-router.delete("/:videoId", deleteVideo);
-router.patch("/:videoId", upload.single("thumbnail"), updateVideo);
-router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
-
-export default router;

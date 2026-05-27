0d46f59104dcb5d635f35cb56874b2e65b322f49
PentagonCoder <harshu33908@gmail.com>
2026-05-27 08:19:04 +0000

    fix: correct formatting of token retrieval in JWT middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ export const verifyJWT =asyncHandler(async (req, res, next) => {
 
   try {
     //AccessToken comes from cookie or authorization header
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    const token = req.cookies?.accessToken  || req.header("Authorization")?.replace("Bearer ", "")
     if (!token) {
       throw new ApiError( 401, "Unauthorized: No token provided User not authenticated");
     }
